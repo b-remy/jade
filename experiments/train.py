d6fbd1d9b44abe8614916cd5aca6c2cdb03fe063
Benjamin Remy <remy.benjamin.101@gmail.com>
2025-11-25 18:38:16 -0600

    fix val loss

--- experiments/train.py
+++ experiments/train.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-os.environ['JAX_PLATFORMS'] = 'cpu'  # or 'gpu' or 'cuda'
 
 import jax
 import jax.numpy as jnp
@@ -22,6 +21,35 @@ import matplotlib.pyplot as plt
 import wandb
 
 from tqdm import tqdm
+
+def plot_denoiser(x, model, key):
+    keys = jax.random.split(key, 2)
+    
+    t = jax.random.beta(keys[0], a=3, b=3, shape=x.shape[:1])
+    sigma_t = sigma_fn(t)
+    
+    # forward diffusion
+    z = sigma_t[...,None,None,None] * jax.random.normal(keys[1], shape=x.shape)
+    xt = x + z
+
+    model = jax.vmap(model, in_axes=(0,0,None,None))
+    x_pred = model(xt, sigma_t, 0, False)  # call methods directly
+
+    fig = plt.figure(figsize=(15, 9))  # Larger figure for 3x5 subplots
+    for i in range(5):
+        for j in range(3):
+            plt.subplot(3, 5, i + 1 + j*5)
+            if j == 0:
+                plt.imshow(x[0, ..., i], cmap='viridis')
+            elif j == 1:
+                plt.imshow(xt[0, ..., i], cmap='viridis')
+            elif j == 2:
+                plt.imshow(x_pred[0, ..., i], cmap='viridis')
+            plt.axis('off')  # Optional: hide axes for cleaner look
+            
+    # Log the figure to wandb
+
+    return fig
 
 def train():
 
@@ -54,38 +82,41 @@ def train():
     weight_fn = lambda t: 1 / sigma_fn(t)**2 + 1
 
     # Start with a classical variance exploding SDE
+    
+    def loss_fn(model, x, key, train=True):
+
+        keys = jax.random.split(key, 2)
+
+        # sample time
+        t = jax.random.beta(keys[0], a=3, b=3, shape=x.shape[:1])
+        sigma_t = sigma_fn(t)
+        
+        # forward diffusion
+        z = sigma_t[...,None,None,None] * jax.random.normal(keys[1], shape=x.shape)
+        xt = x + z
+
+        model = jax.vmap(model, in_axes=(0,0,None,None))
+        x_pred = model(xt, sigma_t, 0, train)  # call methods directly
+
+        # l2 loss
+        loss = (x - x_pred)**2
+
+        return jnp.mean(loss.sum((-1,-2,-3)) * weight_fn(t))
+
     @nnx.jit  # Automatic state management for JAX transforms.
     def train_step(model, optimizer, x, key):
 
         # VE SDE loss function
-        def loss_fn(model):
-
-            keys = jax.random.split(key, 2)
-
-            # sample time
-            t = jax.random.beta(keys[0], a=3, b=3, shape=x.shape[:1])
-            sigma_t = sigma_fn(t)
-            
-            # forward diffusion
-            z = sigma_t[...,None,None,None] * jax.random.normal(keys[1], shape=x.shape)
-            xt = x + z
-
-            model = jax.vmap(model, in_axes=(0,0,None,None))
-            x_pred = model(xt, sigma_t, 0, True)  # call methods directly
-
-            # l2 loss
-            loss = (x - x_pred)**2
-
-            return jnp.mean(loss.sum((-1,-2,-3)) * weight_fn(t))
-
-        loss, grads = nnx.value_and_grad(loss_fn)(model)
+        # loss = loss_fn(model, x, key, train=True)
+        loss_fn_ = partial(loss_fn, x=x, key=key, train=True)
+        loss, grads = nnx.value_and_grad(loss_fn_)(model)
         optimizer.update(model, grads)  # in-place updates
 
         return loss
 
 
     num_epochs = 10
-    batch_size = 32
+    batch_size = 256
 
     key = jax.random.key(0)
 
@@ -101,21 +132,24 @@ def train():
             loss = train_step(model, optimizer, x, subkey)
             run.log({"loss_train": loss})
 
+        
         # Validation
         losses = []
         val_loader = ds_val.iter(batch_size=batch_size, drop_last_batch=False)
         for batch in val_loader:
             x_val = batch["map"]
-            val_loss = loss_fn(params, x_val)
+            #model_ = jax.vmap(model, in_axes=(0,0,None,None))
+            val_loss = loss_fn(model, x_val, key, train=False)
             losses.append(val_loss)
 
         val_loss = np.mean(losses)
-        
         run.log({"val_loss": val_loss})
 
         print(f"Epoch {epoch + 1}, Validation Loss: {val_loss}")
 
-        # wandb.log({"epoch": epoch + 1, "val_loss": val_loss})
+        fig = plot_denoiser(x_val, model, key)
+        wandb.log({"denoiser": wandb.Image(fig)})
+        plt.close(fig)
 
 if __name__ == "__main__":
     # runid = wandb.util.generate_id()
