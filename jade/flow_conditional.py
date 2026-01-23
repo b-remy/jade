eff664d5c60212db1cb6cffe3a23b20d7ea7c363
Benjamin Remy <remy.benjamin.101@gmail.com>
2026-01-23 11:36:36 -0600

    add conditional jade and training script

---
+++ jade/flow_conditional.py
@@ -0,0 +1,134 @@
+import jax
+import jax.numpy as jnp
+from flax import nnx
+from functools import partial
+
+from jade.lensing import Operator
+from jade.diffusion import VESDE
+
+class Denoiser(nnx.Module):
+    def __init__(self, model: nnx.Module, cfg: dict):
+        self.model = model
+        self.cfg = cfg
+
+        self.t_eps = cfg.get('sampling', {}).get('t_eps', 0.05)
+        self.noise_scale = cfg.get('sampling', {}).get('noise_scale', 1.0)
+
+    def __call__(self, x,  cosmo, t, cond=None, train: bool = False):
+        return self.model(x, cosmo, t, cond=cond, train=train)
+
+    def x_pred(self, xt, cosmot, t, cond=None, train: bool = False):
+        return self(xt, cosmot, t, cond=cond, train=train)
+
+    def v_pred(self, xt, cosmot, t, cond=None, train: bool = False):
+        x_pred, cosmo_pred = self.x_pred(xt, cosmot, t, cond=cond, train=train)
+
+        # Compute velocity: v = (x - z) / (1 - t)
+        v_x = (x_pred - xt) / jnp.clip(1.0 - t, a_min=self.t_eps)
+        v_cosmo = (cosmo_pred - cosmot) / jnp.clip(1.0 - t, a_min=self.t_eps)
+
+        return v_x, v_cosmo
+
+    def forward_coupling(self, x, cosmo, t, key):
+        alpha_t = t
+        sigma_t = 1.0 - t
+
+        noise_x = jax.random.normal(key, shape=x.shape)
+        noise_cosmo = jax.random.normal(key, shape=cosmo.shape)
+
+        xt = alpha_t * x + sigma_t * noise_x
+        cosmot = alpha_t * cosmo + sigma_t * noise_cosmo
+
+        return xt, cosmot
+
+class PosteriorDenoiser(Denoiser):
+    def __init__(self, model: nnx.Module, cfg: dict, gamma, sigma_gamma=1.0):
+        self.model = model
+        self.cfg = cfg
+
+        self.t_eps = cfg.get('sampling', {}).get('t_eps', 0.001)
+        self.noise_scale = cfg.get('sampling', {}).get('noise_scale', 1.0)
+
+        # linear solver
+        self.solve = jax.scipy.sparse.linalg.cg
+        #self.solve = jax.scipy.sparse.linalg.gmres
+        #self.solve = jax.scipy.sparse.linalg.bicgstab
+        self.tol = 1e-3
+        self.maxiter = 10
+
+        self.gamma = gamma
+        self.cov_y = sigma_gamma ** 2
+
+    def __call__(self, xt, cosmot, t, train: bool = False):
+
+        alpha_t = t
+        sigma_t = 1.0 - t
+
+        cov_t = sigma_t**2 / jnp.clip(alpha_t, a_min=self.t_eps)
+
+        (x, cosmo), vjp = jax.vjp(lambda x, cosmo: self.model(x, cosmo, t, False), xt, cosmot)
+   
+        y, A = jax.linearize(Operator, x)
+ 
+        At_ = jax.linear_transpose(Operator, x)
+        At = lambda x: next(iter(At_(x)))
+
+        
+        # DPS
+        #cov_y_xt = lambda v: (self.cov_y*v) + cov_t*A(At(v))
+        
+        # MMPS
+        cov_y_xt = lambda v: self.cov_y*v + cov_t*A(vjp((At(v), jnp.zeros(6)))[0])
+
+        b = self.gamma - y
+        
+        v, _ = self.solve(
+            A=cov_y_xt,
+            b=b,
+            tol=self.tol,
+            maxiter=self.maxiter,
+        )
+
+        (score, _) = vjp((At(v), jnp.zeros_like(cosmo)))
+        
+        return x + cov_t * score, cosmo
+        
+        
+class FlowLoss(nnx.Module):
+    def __init__(self, cfg: dict):
+        self.t_eps = cfg["diffusion"]["t_eps"]
+        self.mu = cfg['diffusion']['mu']
+        self.sigma= cfg['diffusion']['sigma']
+
+    def __call__(self, model, x, cosmo, key, lambda_cosmo, train: bool = False, cond=None):
+
+        mu = self.mu
+        sigma = self.sigma
+        s = (jax.random.normal(key, shape=x.shape[:1]) + mu) * sigma
+        t = jax.nn.sigmoid(s)
+
+        alpha_t = t
+        sigma_t = 1 - t
+
+        noise_x = jax.random.normal(key, shape=x.shape)
+        noise_cosmo = jax.random.normal(key, shape=cosmo.shape)
+
+        xt = alpha_t[:,None,None,None] * x + sigma_t[:,None,None,None] * noise_x
+        cosmot = alpha_t[:,None] * cosmo + sigma_t[:,None] * noise_cosmo
+
+        x_pred, cosmo_pred = jax.vmap(model.x_pred, in_axes=(0,0,0,0, None))(xt, cosmot, t, cond, train)
+
+        vx = (x - xt) / jnp.clip((1 - t[:,None,None,None]), a_min=0.05)
+        vx_pred = (x_pred - xt) / jnp.clip((1 - t[:,None,None,None]), a_min=0.05) 
+
+        vcosmo = (cosmo - cosmot) / jnp.clip((1 - t[:,None]), a_min=0.05)
+        vcosmo_pred = (cosmo_pred - cosmot) / jnp.clip((1 - t[:,None]), a_min=0.05)
+        
+        loss_x = jnp.mean((x - x_pred)**2, axis=(-1,-2,-3))
+        loss_cosmo = jnp.mean((cosmo - cosmo_pred)**2, axis=-1)
+
+        total_loss = loss_x + lambda_cosmo * loss_cosmo
+        
+        total_loss = total_loss.mean()
+
+        return total_loss, (jnp.mean(loss_x), jnp.mean(loss_cosmo))
---
+++ jade/sampling_conditional.py
@@ -0,0 +1,142 @@
+import jax
+import jax.numpy as jnp
+
+from flax import nnx
+
+class Sampler(nnx.Module):
+
+    def __init__(self, model, num_steps: int):
+        """
+        model: denoiser
+        x0: initial state
+        num_steps: number of integration steps
+        """
+
+        self.model = model
+        self.num_steps = num_steps 
+
+    def step(self, xt, cosmot, t, t_next, cond=None, key=None):
+        raise NotImplementedError()
+
+    def __call__(self, x0, cosmo0, cond=None, key=None):
+        """
+        x0: initial state
+        """
+
+        timesteps = jnp.linspace(0.0, 1.0, self.num_steps + 1)
+        keys = jax.random.split(key, self.num_steps+1)
+
+        def scan_body(carry, t_pair_key):
+            z_x, z_cosmo = carry
+            t, t_next, key = t_pair_key
+            z_x_new, z_cosmo_new = self.step(z_x, z_cosmo, t, t_next, cond=cond, key=key)
+            return (z_x_new, z_cosmo_new), None
+
+        # Initial state
+        init_carry = (x0, cosmo0)
+
+        # Create pairs of (t, t_next)
+        timestep_pairs_key = (timesteps[:-1], timesteps[1:], keys[:-1])
+
+        # Run the ODE solver
+        (z_x, z_cosmo), _ = jax.lax.scan(scan_body, init_carry, timestep_pairs_key)
+        
+        # Final Euler step
+        t, t_next = timesteps[-2], timesteps[-1]
+
+        v_x, v_cosmo = self.model.v_pred(z_x, z_cosmo, t, cond=cond, train=False)
+
+        dt_x = (t_next - t)
+        dt_cosmo = (t_next - t)
+        
+        z_x = z_x + dt_x * v_x
+        z_cosmo = z_cosmo + dt_cosmo * v_cosmo
+        
+        return z_x, z_cosmo
+
+class EulerSampler(Sampler):
+
+    def step(self, xt, cosmot, t, t_next, cond=None, **kwargs):
+        """
+        xt: current state
+        cosmot: current cosmology
+        t: current time
+        t_next: next time
+        """
+        
+        v_x, v_cosmo = self.model.v_pred(xt, cosmot, t, cond=cond, train=False)
+
+        dt_x = (t_next - t)
+        dt_cosmo = (t_next - t)
+        
+        x_next = xt + dt_x * v_x
+        cosmo_next = cosmot + dt_cosmo * v_cosmo
+        
+        return x_next, cosmo_next
+    
+class HeunSampler(Sampler):
+
+    def step(self, xt, cosmot, t, t_next, **kwargs):
+        """
+        xt: current state
+        cosmot: current cosmology
+        t: current time
+        t_next: next time
+        """
+
+        # First prediction at t
+        v_x_t, v_cosmo_t = self.model.v_pred(xt, cosmot, t, False)
+        
+        dt_x = (t_next - t)
+        dt_cosmo = (t_next - t)
+        
+        # Euler step to get tentative next state
+        x_euler = xt + dt_x * v_x_t
+        cosmo_euler = cosmot + dt_cosmo * v_cosmo_t
+        
+        # Second prediction at t_next
+        v_x_t_next, v_cosmo_t_next = self.model.v_pred(x_euler, cosmo_euler, t_next, False)
+        
+        # Average the two predictions
+        v_x = 0.5 * (v_x_t + v_x_t_next)
+        v_cosmo = 0.5 * (v_cosmo_t + v_cosmo_t_next)
+        
+        x_next = xt + dt_x * v_x
+        cosmo_next = cosmot + dt_cosmo * v_cosmo
+
+        return x_next, cosmo_next
+
+class DDPM(Sampler):
+
+    def step(self, xt, cosmot, t, t_next, key):
+        """
+        xt: current state
+        cosmot: current cosmology
+        t: current time
+        t_next: next time
+        """
+
+        x_pred, cosmo_pred = self.model.x_pred(xt, cosmot, t, False)
+
+        alpha_t = 1.0 - t
+        sigma_t = jnp.clip(t, a_min=self.model.t_eps)
+
+        alpha_t_next = jnp.clip(1.0 - t_next, a_min=self.model.t_eps)
+        sigma_t_next = t_next
+
+        # tau = 1 - (alpha_t / alpha_t_next * sigma_t_next / sigma_t)**2
+        tau = 1 - (alpha_t_next / alpha_t * sigma_t / sigma_t_next)**2
+
+        keys = jax.random.split(key, 2)
+        eps_x = jax.random.normal(keys[0], shape=xt.shape)
+        eps_cosmo = jax.random.normal(keys[1], shape=cosmot.shape)
+
+        x_next = alpha_t_next * x_pred
+        x_next = x_next + sigma_t_next * jnp.sqrt(1 - tau) / sigma_t * (xt - alpha_t * x_pred)
+        x_next = x_next + sigma_t_next * jnp.sqrt(tau) * eps_x
+
+        cosmo_next = alpha_t_next * cosmo_pred
+        cosmo_next = cosmo_next + sigma_t_next * jnp.sqrt(1-tau) / sigma_t * (cosmot - alpha_t * cosmo_pred)
+        cosmo_next = cosmo_next + sigma_t_next * jnp.sqrt(tau) * eps_cosmo
+
+        return x_next, cosmo_next
